e797389c80d27d28b0d879bd4335006dbb718940
Anderson Patiño <andersondavidpc@ufps.edu.co>
2024-12-05 08:41:06 -0500

    feat: actualizar configuración de API para entornos de desarrollo y producción

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     // Development API
-    apiUrl: 'http://localhost:3000/api',
+    // apiUrl: 'http://localhost:3000/api',
     // Production API
-    // apiUrl: 'https://api-express-loe7.onrender.com',
+    apiUrl: 'https://api-express-loe7.onrender.com/api',
   };
   
